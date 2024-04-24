4958e85745aa7121ded77c90837994f58fb2e9a1
Sonny Mijael <sonny.arce@kiubix.com>
2024-04-23 23:18:40 -0600

    Fonts and start project

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import Fonts from './components/fonts.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Fonts />
+    <App />
+  </React.StrictMode>,
+)
